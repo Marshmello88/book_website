6229ede7b311ef25ae3f9d02602ebfafa802bc34
Marshmello88 <m.neibauere@gmail.com>
2020-06-06 17:55:30 +0000

    fixed block content issue

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 #imports
 app = Flask(__name__) 
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -12,6 +13,7 @@ def index():
 @app.route('/account')
 def account():
     return render_template("account.html")
+
 
 # Route for handling the login page logic https://realpython.com/introduction-to-flask-part-2-creating-a-login-page/
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,10 +24,10 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+    return render_template('account.html', error=error)
 
 
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")), debug=True)
+        port=int(os.getenv("PORT", "5000")), debug=False)
