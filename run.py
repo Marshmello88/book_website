c9bf9f59c54c1f87deec16053cce2f3226c9c6bf
Marshmello88 <m.neibauere@gmail.com>
2020-07-11 20:33:50 +0000

    registration template added

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 import os
 import json
 from flask import Flask, render_template, request, flash, redirect, url_for
-#imports
+from bson.objectid import ObjectId
+
 app = Flask(__name__) 
+app.config["MONGO_DBNAME"] = 'plantstoreDB'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 @app.route('/account')
 def account():
